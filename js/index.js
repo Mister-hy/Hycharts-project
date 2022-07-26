15d1dd7a1b295c300b48a8b845bda85d9febd22b
韩洋 <731314840@qq.com>
2022-07-26 15:57:17 +0800

    'create:monitor'

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// 监控区域模块页面制作
+// 立即执行函数
+(function () {
+  $(".monitor .tabs").on("click", "a", function () {
+    $(this).addClass("active").siblings("a").removeClass("active");
+    // 选取对应索引号的content
+    $(".monitor .content")
+      .eq($(this).index())
+      .show()
+      .siblings(".content")
+      .hide();
+  });
+  // 1 先克隆marquee里面所有的行
+  $(".marquee-view .marquee").each(function () {
+    var rows = $(this).children().clone();
+    $(this).append(rows);
+  });
+})();
