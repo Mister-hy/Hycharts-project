087d8a79ce0dc846d5b54287a52d448564eaaf3f
韩洋 <731314840@qq.com>
2022-07-28 19:57:19 +0800

    'create:'HotList''

---
+++ js/Hyecharts/HotList.js
@@ -0,0 +1,142 @@
+// 全国热榜
+(function () {
+  // 1. 准备相关数据
+  var hotData = [
+    {
+      city: "北京", // 城市
+      sales: "25, 179", // 销售额
+      flag: true, //  上升还是下降
+      brands: [
+        //  品牌种类数据
+        { name: "可爱多", num: "9,086", flag: true },
+        { name: "娃哈哈", num: "8,341", flag: true },
+        { name: "喜之郎", num: "7,407", flag: false },
+        { name: "八喜", num: "6,080", flag: false },
+        { name: "小洋人", num: "6,724", flag: false },
+        { name: "好多鱼", num: "2,170", flag: true },
+      ],
+    },
+    {
+      city: "河北",
+      sales: "23,252",
+      flag: false,
+      brands: [
+        { name: "可爱多", num: "3,457", flag: false },
+        { name: "娃哈哈", num: "2,124", flag: true },
+        { name: "喜之郎", num: "8,907", flag: false },
+        { name: "八喜", num: "6,080", flag: true },
+        { name: "小洋人", num: "1,724", flag: false },
+        { name: "好多鱼", num: "1,170", flag: false },
+      ],
+    },
+    {
+      city: "上海",
+      sales: "20,760",
+      flag: true,
+      brands: [
+        { name: "可爱多", num: "2,345", flag: true },
+        { name: "娃哈哈", num: "7,109", flag: true },
+        { name: "喜之郎", num: "3,701", flag: false },
+        { name: "八喜", num: "6,080", flag: false },
+        { name: "小洋人", num: "2,724", flag: false },
+        { name: "好多鱼", num: "2,998", flag: true },
+      ],
+    },
+    {
+      city: "江苏",
+      sales: "23,252",
+      flag: false,
+      brands: [
+        { name: "可爱多", num: "2,156", flag: false },
+        { name: "娃哈哈", num: "2,456", flag: true },
+        { name: "喜之郎", num: "9,737", flag: true },
+        { name: "八喜", num: "2,080", flag: true },
+        { name: "小洋人", num: "8,724", flag: true },
+        { name: "好多鱼", num: "1,770", flag: false },
+      ],
+    },
+    {
+      city: "山东",
+      sales: "20,760",
+      flag: true,
+      brands: [
+        { name: "可爱多", num: "9,567", flag: true },
+        { name: "娃哈哈", num: "2,345", flag: false },
+        { name: "喜之郎", num: "9,037", flag: false },
+        { name: "八喜", num: "1,080", flag: true },
+        { name: "小洋人", num: "4,724", flag: false },
+        { name: "好多鱼", num: "9,999", flag: true },
+      ],
+    },
+  ];
+  // 2 根据数据渲染各省热销 sup 模块内容
+  var supHTML = "";
+  $.each(hotData, function (index, item) {
+    console.log(item);
+    supHTML += `<li><span>${item.city}</span><span> ${item.sales} <s class=
+    ${item.flag ? "icon-up" : "icon-down"}></s></span></li>`;
+  });
+  //将生成的五个小li字符串给 sub dom盒子
+  $(".sup").html(supHTML);
+  // 3. 当鼠标进入 tab 的时候
+  // 鼠标经过当前的小li要高亮显示
+  $(".province .sup").on("mouseenter", "li", function() {
+    // alert(1)
+    index = $(this).index();
+    render($(this));
+  });
+
+// 声明一个函数 里面设置sup当前小li高亮还有 对应的品牌对象渲染
+  // 这个函数需要传递当前元素进去
+  function render(currentEle) {
+    currentEle
+      .addClass("active")
+      .siblings()
+      .removeClass();
+    // 拿到当前城市的品牌对象
+    // console.log($(this).index());
+    // 可以通过hotData[$(this).index()] 得到当前的城市
+    // console.log(hotData[$(this).index()]);
+    // 我们可以通过hotData[$(this).index()].brands 拿到的是城市对象的品牌种类
+    // console.log(hotData[$(this).index()].brands);
+    // 开始遍历品牌数组
+    var subHTML = "";
+    $.each(hotData[currentEle.index()].brands, function(index, item) {
+      // 是对应城市的每一个品牌对象
+      // console.log(item);
+      subHTML += `<li><span>${item.name}</span><span> ${item.num}<s class=
+    ${item.flag ? "icon-up" : "icon-down"}
+    ></s></span></li>`;
+    });
+    // 把生成的6个小li字符串给 sub dom盒子
+    $(".sub").html(subHTML);
+  }
+  // 4. 默认把第一个小li处于鼠标经过状态
+  var lis = $(".province .sup li");
+  lis.eq(0).mouseenter();
+  // 5 开启定时器
+  var index = 0;
+  var timer = setInterval(function() {
+    index++;
+    if (index >= 5) index = 0;
+    // lis.eq(index).mouseenter();
+    render(lis.eq(index));
+  }, 2000);
+
+  $(".province .sup").hover(
+    // 鼠标经过事件
+    function() {
+      clearInterval(timer);
+    },
+    // 鼠标离开事件
+    function() {
+      clearInterval(timer);
+      timer = setInterval(function() {
+        index++;
+        if (index >= 5) index = 0;
+        // lis.eq(index).mouseenter();
+        render(lis.eq(index));
+      }, 2000);
+    }
+  );
+})();
